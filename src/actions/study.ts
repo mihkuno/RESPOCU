643a35c7e14dbba6a0949b32978ae8fd2d6ceb12
mihkuno <caindayjoeninyo@gmail.com>
2025-04-06 04:33:44 +0800

    feat: implement bookmark, archive, and study management functions in study actions

---
+++ src/actions/study.ts
@@ -0,0 +1,99 @@
+"use server";
+import { connectDatabase } from "@/lib/database";
+import Research from "@/models/Research";
+import { revalidatePath } from "next/cache";
+
+export async function addBookmark(id: string, email: string) {
+    await connectDatabase();
+
+    await Research.findByIdAndUpdate(
+        id,
+        {
+            $addToSet: { bookmarked_by: email },
+        },
+        { new: true }
+    );
+
+    revalidatePath("/");
+}
+
+export async function removeBookmark(id: string, email: string) {
+    await connectDatabase();
+
+    await Research.findByIdAndUpdate(
+        id,
+        {
+            $pull: { bookmarked_by: email },
+        },
+        { new: true }
+    );
+
+    revalidatePath("/");
+}
+
+export async function storeArchive(id: string) {
+    
+    // TODO: validate the access token for this action
+    await connectDatabase();
+
+    await Research.findByIdAndUpdate(
+        id,
+        { is_archived: true },
+        { new: true }
+    );
+
+    revalidatePath("/");
+}
+
+
+export async function restoreArchive(id: string) {
+    
+    // TODO: validate the access token for this action
+    await connectDatabase();
+
+    await Research.findByIdAndUpdate(
+        id,
+        { is_archived: false },
+        { new: true }
+    );
+
+    revalidatePath("/");
+}
+
+export async function markBestPaper(id: string) {
+
+    // TODO: validate the access token for this action
+    await connectDatabase();
+
+    await Research.findByIdAndUpdate(
+        id,
+        { is_best: true },
+        { new: true }
+    );
+
+    revalidatePath("/");
+}
+
+export async function unmarkBestPaper(id: string) {
+
+    // TODO: validate the access token for this action
+    await connectDatabase();
+
+    await Research.findByIdAndUpdate(
+        id,
+        { is_best: false },
+        { new: true }
+    );
+
+    revalidatePath("/");
+}
+
+export async function deleteStudy(id: string) {
+
+    // TODO: validate the access token for this action
+    await connectDatabase();
+
+    await Research.findByIdAndDelete(id);
+
+    revalidatePath("/");
+}
