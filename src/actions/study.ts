bad75652984d636eb2560d702d5995f379a52704
mihkuno <caindayjoeninyo@gmail.com>
2025-04-07 08:56:46 +0800

    feat: enhance viewStudies and viewArchived functions to return structured study data

--- src/actions/study.ts
+++ src/actions/study.ts
@@ -3,6 +3,58 @@ import { connectDatabase } from "@/lib/database";
 import Research from "@/models/Research";
 import { revalidatePath } from "next/cache";
 
+
+
+export async function viewStudies(email: string) {
+    // TODO: validate the access token for this action
+
+    await connectDatabase();
+    
+    let studies = await Research.find({ is_archived: false }).select("-file");
+    if (studies) {
+        studies = studies.map((study) => ({
+            id: study._id.toString(),
+            title: study.title,
+            description: study.description,
+            categories: study.categories,
+            type: study.research_type,
+            authors: study.authors,
+            publishedBy: study.publisher,
+            publishedDate: study.created_at,
+            isBestPaper: study.is_best,
+            isArchived: study.is_archived,
+            isBookmarked: study.bookmarked_by.includes(email),
+        }));
+    }
+
+    return studies;
+}
+
+export async function viewArchived(email: string) {
+    // TODO: validate the access token for this action
+
+    await connectDatabase();
+    
+    let studies = await Research.find({ is_archived: true }).select("-file");
+    if (studies) {
+        studies = studies.map((study) => ({
+            id: study._id.toString(),
+            title: study.title,
+            description: study.description,
+            categories: study.categories,
+            type: study.research_type,
+            authors: study.authors,
+            publishedBy: study.publisher,
+            publishedDate: study.created_at,
+            isBestPaper: study.is_best,
+            isArchived: study.is_archived,
+            isBookmarked: study.bookmarked_by.includes(email),
+        }));
+    }
+
+    return studies;
+}
+
 export async function addBookmark(id: string, email: string) {
     // TODO: validate the access token for this action
 
